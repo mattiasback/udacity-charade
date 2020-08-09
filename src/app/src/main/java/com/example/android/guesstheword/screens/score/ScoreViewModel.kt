6fe72c7755f7ca590256ca2e9f92c693c9063edd
mattiasback <mattias.j.back@gmail.com>
2020-08-09 22:58:27 +0200

    Move data from fragment to LiveData in ViewModel

--- src/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ src/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -1,10 +1,31 @@
 package com.example.android.guesstheword.screens.score
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ScoreViewModel(finalScore: Int) : ViewModel() {
+
+    //Final score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    //Play again event
+    private val _playAgainEvent = MutableLiveData<Boolean>()
+    val playAgainEvent: LiveData<Boolean>
+        get() = _playAgainEvent
+
     init {
-        Log.i("SomeViewModel", "Final score is $finalScore")
+        _score.value = finalScore
+    }
+
+    fun onPlayAgain() {
+        _playAgainEvent.value = true
+    }
+
+    fun onPlayAgainComplete() {
+        _playAgainEvent.value = false
     }
 }
